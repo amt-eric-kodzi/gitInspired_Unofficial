432a11759b20cd5c40044a4a882c8ed748abf244
eric kodzi <eric.kodzi@amalitech.org>
2023-07-06 12:41:08 +0000

    feat: hacked

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -2,15 +2,15 @@ import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
 import { AdminDashboard } from '../layouts/AdminDashboard';
 import { LecturerDashboard } from '../layouts/LecturerDashboard';
-//import { Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import StudentDashboard from '../layouts/StudentDashboard';
 
 export const Dashboard = () => {
   const user = useSelector((state: RootState) => state.auth.user);
   return (
     <>
-      {/* {!user?.isVerified && <Navigate to='/resetpassword' />} */}
-      {/* user?.isVerified && user?.role === 'ADMIN' &&  */<AdminDashboard></AdminDashboard>}
+      {!user?.isVerified && <Navigate to='/resetpassword' />}
+      {user?.isVerified && user?.role === 'ADMIN' && <AdminDashboard></AdminDashboard>}
       {user?.isVerified && user?.role === 'LECTURER' && <LecturerDashboard></LecturerDashboard>}
       {user?.isVerified && user?.role === 'STUDENT' && <StudentDashboard></StudentDashboard>}
     </>
